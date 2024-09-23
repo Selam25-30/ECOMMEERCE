81ab26527cf6082f7b5e685f645f49b2a370c982
Selam25-30 <selamawitbirhanu123@gmail.com>
2024-09-23 06:40:47 -0700

    Authentication

--- amazonclone/src/Pages/Auth/Auth.jsx
+++ amazonclone/src/Pages/Auth/Auth.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import classes from 'SignUp.module.css';
+import classes from '../Auth/SignUp.module.css';
 // import LayOut from "../../Components/LayOut/LayOut";
 function Auth() {
   return (
