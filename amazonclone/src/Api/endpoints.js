567e779e6d615700095c0c4b27d86d08a10485e9
Selam25-30 <selamawitbirhanu123@gmail.com>
2024-09-26 00:52:37 -0700

    finilised

--- amazonclone/src/Api/endpoints.js
+++
@@ -1,4 +0,0 @@
-const productUrl = "https://fakestoreapi.com"
-
-
-export default productUrl
--- amazonclone/src/Components/Carousel/data.js
+++
@@ -1,9 +0,0 @@
-import img1 from './img/1001.png'
-import img2 from './img/1002.png'
-import img3 from './img/1003.png'
-import img4 from './img/1004.png'
-import img5 from './img/1005.png'
-
-
-
-export const img= [img1,img2,img3,img4,img5]
