c45670a9d91efea48af497b5ef55aa7bb4b9e2d5
Selamawit Birhanu Tabor <99592520+Selam25-30@users.noreply.github.com>
2025-02-17 09:30:32 +0300

    Finalized

--- src/Components/Catagory/CategoryCard.jsx
+++ src/Components/Catagory/CategoryCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import classes from "./Category.module.css";
 import { Link } from "react-router-dom";
 
+
 function CategoryCard({data}) {
   return (
     <div className={classes.category}>
